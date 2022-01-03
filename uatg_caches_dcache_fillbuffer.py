10dcf315e4f3f458336b8768273af8975faf3342
charithaaradhya <60753430+charithaaradhya@users.noreply.github.com>
2022-01-03 13:50:26 +0530

    Update uatg_caches_dcache_fillbuffer.py

--- uatg_caches_dcache_fillbuffer.py
+++ uatg_caches_dcache_fillbuffer.py
@@ -27,25 +27,21 @@ class uatg_caches_dcache_fillbuffers(IPlugin):
 		return True
         	
         def generate_asm(self) -> List[Dict[str, Union[Union[str, list], Any]]]:
-        	asm_main="\n\tfence\n\tli t0, 77\n\tli t3, {0}\n\tla t2, rvtest_data" .format(self.sets * self.ways)
-        	asm_loop1="\n\tloop1:\n\tsw t0, {0}(t2)\n\taddi t2, t2,64\n\tbeq t4, t3, asm_nop\n\taddi t4, t4, 1\n\tj loop1\n\t".format(self.word_size * self.block_size)
-        	asm_nop="\n\tnop\n\t"
-       		for i in range(self.fb_size * 2):
-            		asm_loop2 +="\tsw t0, {0}(t2)\n".format(32 * (i + 1))
-            	asm_end="\n\tend:\n\tnop\n\tfence.i\n\t"
-            	
-            	asm_code=asm_main+asm_loop1+asm_nop+asm_loop2+asm_end
-            	compile_macros=[]
-            	
+        	asm_main="\n\tli t1, 77\n\tli t3, {0}\n\tla t2, rvtest_data\n" .format(self.sets * self.ways)
+        	asm_loop1="loop1:\n\tsd  t1,t8(x10)\n\taddi  t8,t8,64\n\tbeq  t8,t4,loop2\n\tj loop1\n"
+        	asm_loop="loop2:\n\taddi t3,t3,1\n\tli t8,0\n\tbeq  t3,{0},end\n\tj loop1\n".format(self.sets)
+        	asm_nop="asm_nop:\nnop\n\t"
+        	asm_loop2=""
+        	for i in range(self.fb_size ):
+        		asm_loop2 += "\nsw t1, {0}(t2)\n".format(32 * (i + 1))
+        	asm_end="\nend:\n\tnop\n\tfence.i\n\t"
+        	asm_code=asm_main+asm_loop1+asm_loop+asm_nop+asm_loop2+asm_end
+        	compile_macros=[]
         	asm_data = '\nrvtest_data:\n'
-                
-                for i in range (self.block_size * self.sets * self.ways*2):
-            		asm_data += "\t.word 0x{0:08x}\n".format(random.randrange(16**8))
-
-                return[{'asm_code': asm_code,
-                    'asm_data': asm_data,
-                    'asm_sig': '',
-                    'compile_macros': compile_macros}]
+        	for i in range (self.block_size * self.sets * self.ways*2):
+        		asm_data += "\t.word 0x{0:08x}\n".format(random.randrange(2**32))
+        	return[{'asm_code': asm_code,'asm_data': asm_data,'asm_sig': '','compile_macros': compile_macros}]
+                    
                     
          def check_log(self, log_file_path, reports_dir)-> Bool:
       		return False
